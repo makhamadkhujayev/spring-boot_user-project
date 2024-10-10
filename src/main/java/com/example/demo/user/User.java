e78ff712b4c299fcf96c730eff188ff3cc3cae94
Акбархужа Махамадхужаев <akbarhuzamahamadhuzaev@MacBook-Air-Akbarhuza.local>
2024-10-10 16:48:02 +0500

    initial commit

---
+++ src/main/java/com/example/demo/user/User.java
@@ -0,0 +1,80 @@
+package com.example.demo.user;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String name;
+    @Transient
+    private Integer age;
+    private LocalDate dob;
+
+    public User(String email, String name, LocalDate dob) {
+        this.email = email;
+        this.name = name;
+        this.dob = dob;
+    }
+
+    public User() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return Period.between(this.dob, LocalDate.now()).getYears();
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", dob=" + dob +
+                '}';
+    }
+}
